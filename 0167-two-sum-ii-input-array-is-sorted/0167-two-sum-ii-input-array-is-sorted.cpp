8b49102313910cdd734c139e12c31f001b60bc34
Dhawal <89371061+DhawalSri@users.noreply.github.com>
2026-08-03 23:04:41 +0530

    Time: 0 ms (100%), Space: 19.6 MB (34.47%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
+        vector<int> ans;
         int n = numbers.size();
         int left = 0;
         int right = n-1;
@@ -12,11 +13,14 @@ public:
                 right--;
             }
             else{
-                return {left+1, right+1};
+                // return {left+1, right+1};
+                ans.push_back(left+1);
+                ans.push_back(right+1);
+                break;
             }
 
         }
 
-        return {};
+        return ans;
     }
 };
